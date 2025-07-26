5b603546e8789885b610fd9812721bc610d8d0b6
FrankMerriman <68456230+FrankMerriman@users.noreply.github.com>
2025-07-26 14:47:28 +1000

    updated main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 picam2 = Picamera2()
 camera_config = picam2.create_still_configuration()
+print(camera_config)
 picam2.configure(camera_config)
 picam2.start()
 picam2.capture_file("test.jpg")
