8c330c5ed0d93dc40b4e35403bd4240079e2a417
FrankMerriman <68456230+FrankMerriman@users.noreply.github.com>
2025-12-24 17:11:56 +1100

    added rotation

--- screens/cameraScreen.py
+++ screens/cameraScreen.py
@@ -39,8 +39,8 @@ class CameraScreen:
     def preview_camera(self):
         frame = self.picam2.capture_array()
         small_frame = frame[::2, ::2, :]  # 640x480 -> 320x240
-        small_frame = np.ascontiguousarray(np.rot90(small_frame, k=1))
-        small_frame = self.draw_ui(small_frame)
+        # small_frame = np.ascontiguousarray(np.rot90(small_frame, k=1))
+        # small_frame = self.draw_ui(small_frame)
     
         # Need to convert to RGB565 for the framebuffer, otherwise screen is garbled noise
         fb_bytes = rgb24_to_rgb565(small_frame)
