92fd7e58f7c0785c1e384cc3835b11771e21771b
FrankMerriman <68456230+FrankMerriman@users.noreply.github.com>
2025-12-25 20:41:50 +1100

    fixed mount issues

--- utils/mountUSB.py
+++ utils/mountUSB.py
@@ -11,6 +11,19 @@ def mount_usb():
 
         usb_device = "/dev/sda1"
 
+        result = subprocess.run(["mount"], capture_output=True, text=True, check=True)
+
+        mounted = False
+        for line in result.stdout.splitlines():
+            if usb_device in line:
+                mounted = True
+                break
+
+        if mounted: # Needs to be fixed to actually check if mount point is diff, but for now stops bugs
+            print(f"USB already mounted at {mount_point}")
+            return mount_point
+
+
         # Mount
         subprocess.run(["sudo", "mount", "-o", "uid=1000,gid=1000", usb_device, str(mount_point)], check=True)
         print(f"USB mounted at {mount_point}")
