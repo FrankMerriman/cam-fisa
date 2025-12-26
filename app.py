e737d2b642c383de78c22633054a5ea5730ee933
FrankMerriman <68456230+FrankMerriman@users.noreply.github.com>
2025-12-26 13:38:55 +1100

    gallery button logic

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 import screens.cameraScreen as cameraScreen
+import screens.galleryScreen as galleryScreen
+from screens.screenManager import ScreenTypes, ScreenManager
+from gpiozero import Button
 
-if __name__ == "__main__":
-    cam_screen = cameraScreen.CameraScreen()
-    cam_screen.start_camera()
-    try:
-        while True:
-            cam_screen.preview_camera()
-    except KeyboardInterrupt:
-        print("Exiting...")
-    finally:
-        cam_screen.stop_camera()
+
+def on_screen_button_pressed():
+    global screen_button_locked
+    if not screen_button_locked:
+        screen_button_locked = True
+        swap_screen()
+
+def on_screen_button_released():
+    global screen_button_locked
+    screen_button_locked = False
+
+def swap_screen():
+    if current_screen == ScreenTypes.PREVIEW:
+        current_screen = ScreenTypes.GALLERY
+    elif current_screen == ScreenTypes.GALLERY:
+        current_screen = ScreenTypes.PREVIEW
+
+screen_button = Button(26, bounce_time=0.05)
+screen_button_locked = False
+
+screen_button.when_pressed = on_screen_button_pressed
+screen_button.when_released = on_screen_button_released
+
+current_screen = ScreenTypes.PREVIEW # Default
+cam_screen = cameraScreen.CameraScreen()
+gallery_screen = galleryScreen.GalleryScreen()
+
+try:
+    while True:
+            if current_screen == ScreenTypes.PREVIEW:
+                cam_screen.preview_camera()
+            elif current_screen == ScreenTypes.GALLERY:
+                print("Switching to camera Gallery Screen")
+                exit()
+            
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    cam_screen.stop_camera()
--- screens/galleryScreen.py
+++ screens/galleryScreen.py
@@ -1,3 +1,18 @@
 # This file lays out the functionality of the gallery screen.
 # It shows previously capture images @ 320x240 with the ability to scroll left to right
 # You can also zoom in and out of images. Zooming out far enough displays a grid of multiple images.
+from utils.mountUSB import mount_usb
+from gpiozero import Button
+
+class GalleryScreen:
+    def __init__(self):
+        self.usb_path = mount_usb()
+        if self.usb_path:
+            self.gallery_path = self.usb_path / "gallery"
+            self.gallery_path.mkdir(exist_ok=True)
+        else:
+            raise RuntimeError("No USB drive found. Cannot create gallery.")
+        
+        self.right_button = Button(26, bounce_time=0.05)  # small debounce
+        self.left_button = Button(26, bounce_time=0.05)  # small debounce
+        
