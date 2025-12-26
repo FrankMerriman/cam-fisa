ad7bec62a1771422b6b32700f50e54ab7ddfbc46
FrankMerriman <68456230+FrankMerriman@users.noreply.github.com>
2025-12-26 14:31:41 +1100

    screens now have better loop logic

--- screens/galleryScreen.py
+++ screens/galleryScreen.py
@@ -5,16 +5,22 @@ from utils.mountUSB import mount_usb
 from gpiozero import Button
 
 class GalleryScreen:
-    FB_PATH = "/dev/fb0"
-    FB_W, FB_H = 240, 320
-
     def __init__(self):
         # Once I have a function for it, do a check for USB mount
         # For now we can assume it is working
-        self.right_button = Button(24, bounce_time=0.05)  # small debounce
-        self.left_button = Button(23, bounce_time=0.05)  # small debounce
+        self.right_button = Button(24, bounce_time=1)  # small debounce
+        self.left_button = Button(23, bounce_time=1)  # small debounce
         self.gallery_path = mount_usb() / "gallery"
         self.images = []
+        self.gallery_lock = False
+
+    def on_right_button_pressed(self):
+        self.gallery_lock = False
+        self.index += 1
+
+    def on_left_button_pressed(self):
+        self.gallery_lock = False
+        self.index -= 1
         
     def load_gallery_images(self):
         for img_file in self.gallery_path.glob("*.jpg"):
@@ -23,19 +29,17 @@ class GalleryScreen:
         self.modulo = len(self.images)
         self.index = len(self.images) - 1  # Start at the most recent image
 
-    def show_gallery(self):
-        
-        if self.modulo == 0:
-            print("No images in gallery.")
-            return
+    def show_image(self):
+        # Gallery lock is meant to stop redrawing of the same image.
+        # Only redraw when the index changes, as lock is released only by button press
+        if not self.gallery_lock:
+            self.gallery_lock = True 
+            
+            # if self.modulo == 0:
+            #     print("No images in gallery.")
+            #     return
 
-        current_image_path = self.images[self.index % self.modulo]
-        print(f"Displaying image: {current_image_path}")
-        # fb_bytes = rgb24_to_rgb565(np.ascontiguousarray(fb_frame))
-        # write_to_screen(self.fb, fb_bytes)
-
-        # Wait for button press to navigate
-        if self.right_button.is_pressed:
-            self.index += 1
-        elif self.left_button.is_pressed:
-            self.index -= 1
+            current_image_path = self.images[self.index % self.modulo]
+            print(f"Displaying image: {current_image_path}")
+            # fb_bytes = rgb24_to_rgb565(np.ascontiguousarray(fb_frame))
+            # write_to_screen(self.fb, fb_bytes)
