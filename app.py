9e06ec5b8ba68fd58b52fcca0b2bb5dfc7683bc6
FrankMerriman <68456230+FrankMerriman@users.noreply.github.com>
2025-12-27 11:01:04 +1100

    removed shutdown button as I swap to read only FS

--- app.py
+++ app.py
@@ -12,16 +12,10 @@ cam_screen = CameraScreen(fb)
 gallery_screen = GalleryScreen(fb)
 screen_manager = ScreenManager(fb, cam_screen, gallery_screen)
 
-shutdown_button = Button(4)
-
 try:
     while True:
         active_screen = screen_manager.get_active_screen()
         active_screen.process()
-        if shutdown_button.is_pressed:
-            print("Shutdown button pressed, shutting down...")
-            subprocess.run(["sudo", "shutdown"])
-            exit()
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
